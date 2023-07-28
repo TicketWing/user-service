c9d867dad83bd7f24d14c73b3d65e84f6007302e
AidXylelele <warboyka48@gmail.com>
2023-07-28 23:52:09 +0200

    fix: Fixed knexfile.ts

--- knexfile.ts
+++ knexfile.ts
@@ -1,16 +1,20 @@
-module.exports = {
+require("ts-node/register");
+
+const config = {
   development: {
-    client: 'mysql2',
-    version: '8.0.33',
+    client: "mysql2",
+    version: "8.0.33",
     connection: {
-      host: 'ticketwing-database',
+      host: "ticketwing-database",
       port: 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     },
     migrations: {
-      directory: './src/migrations',
+      directory: "./src/migrations",
     },
   },
 };
+
+export default config;
